1f618d420841d9a9d27ca5949cb6dca854ce1dd1
iperezgamiz <144547977+iperezgamiz@users.noreply.github.com>
2024-03-17 19:19:31 -0400

    Delete api directory

--- api/user/logout.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource, reqparse
-
-class LogoutUser(Resource):
-
-    def get(self, username):
-
-        return {"code": 200, "message": f"User {username} successfully logged out"}
